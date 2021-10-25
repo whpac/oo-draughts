57beb71c4e2ab4b94d67ccb2055d06382ec5aaf5
Marcin Szwarc <marcinszwarc@hotmail.com>
2021-10-25 08:53:09 +0200

    Updated the main code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,25 @@
 #include "board/draughts_board.hpp"
-#include "view/console.hpp"
+#include "view/console_view.hpp"
+#include "input/console_input.hpp"
+#include "input/commands/move_command.hpp"
 
 int main(){
-    Board board = DraughtsBoard(8);
-    Console console;
+    DraughtsBoard board = DraughtsBoard(8);
+    ConsoleView console;
+    ConsoleInput input;
+
+    Command cmd = input.getCommand();
+    switch(cmd.getType()){
+        case CommandType::move:
+            MoveCommand mv_cmd = (MoveCommand)cmd;
+            board.movePawn(mv_cmd.getFrom(), mv_cmd.getTo());
+            break;
+        case CommandType::undo:
+            break;
+        case CommandType::unknown:
+            break;
+    }
+
     console.displayBoard(board);
     return 0;
 }
